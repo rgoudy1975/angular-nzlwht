3e8639f7a705e254a5b4c0f8b10c0646b2c6e72f
rgoudy1975 <roy.goudy@gmail.com>
2019-09-04 14:27:17 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-nzlwht
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-nzlwht)
---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Lorem Ipsum',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Dolor Amet',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Consecutur',
+    price: 299,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
